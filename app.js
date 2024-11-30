5bc222365ce8415cda954b3e7e66b3337f3734f7
HARISH <harishkr0055@gmail.com>
2024-11-30 19:27:06 +0530

    form handling with get method

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const app = express();
 
 app.set("view engine", "ejs");
 
+// This code allows you to access the data entered by user in your console. 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
 app.get("/", (req, res) =>{
     res.render("index");
 });
 
-app.get("/profile", (req, res) => {
-    res.render("profile");
-})
+app.get("/register", (req, res) => {
+
+    // If method -> GET -> req.query 
+    // If method -> POST -> req.body
+
+    console.log(req.query);
+    res.send("User Registered.");
+});
+
 
 // If no route not found then this code will work
 app.get("*", (req, res) => {
