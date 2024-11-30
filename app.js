96cc03e38821d32e2921f2041780aa55a81f2217
HARISH <harishkr0055@gmail.com>
2024-11-30 16:53:40 +0530

    understanding express-sessions, and flash messages

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
 const express = require("express");
 const app = express();
+const expressSession = require("express-session");
+const flash = require("connect-flash");
+
+// Flash -> getting data on a route and using it on some other route.
 
 
-//Middlewares -> middleware is a function that modifies the request and response objects or performs some action before passing control to the next function in the stack.
+// Cookies -> For saving data on Browsers.
+// Express Session -> For Saving data on Server.
 
-// This middleware runs before hitting every route.
 
-app.use("*", (req, res, next) => {
-    console.log("running middleware");
-    next();
-})
+// Setting up middlewares here
+
+app.use(flash());
+app.use( expressSession({
+    secret: "randdom stuffs", 
+    resave: false,
+    saveUninitialized: false,
+}))
 
 app.get("/", (req, res) =>{
     res.send("<h1>This is HOME Page.</h1>");
 });
 
-app.get("/contact", (req, res) => {
-    res.send("<h1>This is Contact Page.</h1>");
+// Saving a cookie on browser
+app.use("/create", (req, res, next) => {
+    req.session.polo = true;
+    res.send("done");
 });
+
+// Redirecting user on a error page
+app.get("/message", (req, res, next) => {
+    req.flash("error", "This is an error message");
+    res.redirect("/error");
+})
+
+app.get("/error", (req, res, next) => {
+    let message = req.flash("error");
+    res.send(message);
+} )
+
+
 
 
 app.listen(3000, ()=>{
